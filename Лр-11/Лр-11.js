950d2d0081dab22d01b12de5c0b49ea8ac0ca791
Your Name <you@example.com>
2026-01-28 15:19:44 +0300

    a

---
+++ Лр-11/Лр-11.js
@@ -0,0 +1,7 @@
+// Use the first element in the collection
+const btn1 = document.getElementsByClassName("myButtonClass")[0];
+btn1.addEventListener("click", myFunction);
+
+function buttonHandler(){
+    console.log("кнопка работает!")
+}
